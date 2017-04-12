f6142f56df0deb4b5c149827c2052036e0d19491
bgveenstra <b.g.veenstra@gmail.com>
2017-04-12 11:02:22 -0700

    rm character parts; add some console logs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -34,52 +34,15 @@ $(document).ready(function(){
     });
   });
 
-
-  $booksList.on('submit', '#addCharacterForm', function(e) {
-    e.preventDefault();
-    console.log('new characters');
-    $.ajax({
-      method: 'POST',
-      url: '/api/books/'+$(this).attr('data-id')+'/characters',
-      data: $(this).serializeArray(),
-      success: newCharacterSuccess,
-      error: newCharacterError
-    });
-  });
-
 });
-
-function getCharacterHtml(_book_id, character) {
-  return `${character.name} <button class="deleteCharacter btn btn-danger" data-bookid=${_book_id} data-charid=${character._id}><b>x</b></button>`;
-}
-
-function getAllCharactersHtml(_book_id, characters) {
-  if (characters) {
-    return characters.map(function(character) {
-                return getCharacterHtml(_book_id, character);
-              }).join("");
-  } else {
-    return "";
-  }
-}
 
 function getBookHtml(book) {
   return `<hr>
           <p>
             <b>${book.title}</b>
-            by ${(book.author) ? book.author.name : 'null'}
-            <br>
-            <b>Characters:</b>
-            ${getAllCharactersHtml(book.id, book.characters)}
+            by ${book.author.name}
             <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${book._id}>Delete</button>
-          </p>
-          <form class="form-inline" id="addCharacterForm" data-id=${book._id}>
-            <div class="form-group">
-              <input type="text" class="form-control" name="name" placeholder="Book character">
-            </div>
-            <button type="submit" class="btn btn-default">Add character</button>
-          </form>
-          `;
+          </p>`;
 }
 
 function getAllBooksHtml(books) {
@@ -104,7 +67,7 @@ function handleSuccess(json) {
 }
 
 function handleError(e) {
-  console.log('uh oh');
+  console.log('uh oh - there was an ajax error');
   $('#bookTarget').text('Failed to load books, is the server working?');
 }
 
@@ -114,14 +77,16 @@ function newBookSuccess(json) {
   render();
 }
 
-function newBookError() {
+function newBookError(xhr, textStatus, errorType) {
   console.log("new book error!");
+  console.log(`${textStatus} - ${errorType}`);
 }
 
 function deleteBookSuccess(json) {
   var book = json;
+  console.log(json);
   var bookId = book._id;
-
+  console.log('delete book', bookId);
   // find the book with the correct ID and remove it from our allBooks array
   for(var index = 0; index < allBooks.length; index++) {
     if(allBooks[index]._id === bookId) {
@@ -134,22 +99,4 @@ function deleteBookSuccess(json) {
 
 function deleteBookError() {
   console.log("book deleting error!");
-}
-
-
-function newCharacterSuccess(json) {
-  var book = json;
-  var bookId = book._id;
-  // find the book with the correct ID and update it
-  for(var index = 0; index < allBooks.length; index++) {
-    if(allBooks[index]._id === bookId) {
-      allBooks[index] = book;
-      break;  // we found our book - no reason to keep searching (this is why we didn't use forEach)
-    }
-  }
-  render();
-}
-
-function newCharacterError() {
-  console.log('adding new character error!');
 }
