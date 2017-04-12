3a0c30ca2d68ee696ea5d512fbf54f35352dff94
bgveenstra <b.g.veenstra@gmail.com>
2017-04-12 11:05:24 -0700

    better error logging; request logging middleware

--- server.js
+++ server.js
@@ -9,12 +9,25 @@ var express = require('express'),
 // generate a new express app and call it 'app'
 var app = express();
 
+// MIDDLEWARE
 // serve static files in public
 app.use(express.static('public'));
 
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// custom middleware to console.log some helpful information
+//   in terminal every time we get a request
+function logRequestInfo(req, res, next){
+  console.log(`\nRECEIVED REQUEST : ${req.method} ${req.url}`);
+  console.log('  url params:', req.params);
+  console.log('  query params:', req.query);
+  console.log('  body:', req.body);
+  next();
+}
+app.use(logRequestInfo);
+
+// ROUTES
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
@@ -27,14 +40,14 @@ app.get('/api/books', function (req, res) {
     .populate('author')
     .exec(function(err, books){
       if (err) {
-        res.status(500).send(err);
-        return;
+        console.log(err.message);
+        res.status(500).send();
+      } else {
+        res.json(books);
       }
-      res.json(books);
     });
 
 });
-
 
 
 app.get('/api/books/:id', function (req, res) {
@@ -44,16 +57,16 @@ app.get('/api/books/:id', function (req, res) {
     .populate('author')
     .exec(function(err, book){
       if (err) {
-        res.status(500).send(err);
-        return;
+        console.log(err.message);
+        res.status(500).send();
+      } else {
+        res.json(book);
       }
-      res.json(book);
     });
-
 });
 
-
-
+// create new book
+// AUTHOR MUST EXIST ALREADY!
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
   var newBook = new db.Book({
@@ -64,19 +77,32 @@ app.post('/api/books', function (req, res) {
   // find the author from req.body
   db.Author.findOne({name: req.body.author}, function(err, author){
     if (err) {
-      return console.log(err);
-    }
-    // add this author to the book
-    newBook.author = author;
-    // save newBook to database
-    newBook.save(function(err, book){
-      if (err) {
-        return console.log("save error: " + err);
+      console.log(err.message);
+      res.status(500).send();
+    } else {
+      console.log('author is:', author);
+      if (author === null){
+        // this author doesn't exist in our database yet
+        // let's log an informative error message
+        console.log(`book create error: author ${req.body.author} not found - create author first!`);
+        res.status(404).send();
+      } else {
+        // found the author!
+        // add this author to the book
+        newBook.author = author;
+        // save newBook to database
+        newBook.save(function(err, book){
+          if (err) {
+            console.log('book save error:', err.message);
+            res.status(500).send();
+          } else {
+            console.log('saved book:', book );
+            // send back the book!
+            res.json(book);
+          }
+        });
       }
-      console.log("saved ", book.title);
-      // send back the book!
-      res.json(book);
-    });
+    }
   });
 
 });
@@ -89,7 +115,12 @@ app.delete('/api/books/:id', function (req, res) {
   var bookId = req.params.id;
 
   db.Book.findOneAndRemove({ _id: bookId }, function (err, deletedBook) {
-    res.json(deletedBook);
+    if (err) {
+      console.log('book delete error:', err.message);
+      res.status(500).send();
+    } else {
+      res.json(deletedBook);
+    }
   });
 });
 
