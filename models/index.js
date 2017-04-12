a4128814633a1d7911c4e5102664bb341ec75537
bgveenstra <b.g.veenstra@gmail.com>
2017-04-12 11:01:51 -0700

    use default js promise library for mongoose to avoid warnings

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
 
+// Use native JavaScript promises to avoid warning
+mongoose.Promise = global.Promise;
+
 module.exports.Book = require("./book.js");
 module.exports.Author = require("./author.js");
