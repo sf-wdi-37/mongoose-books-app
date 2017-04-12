0e915720f123fb582029a876f217920cc3bec2cc
bgveenstra <b.g.veenstra@gmail.com>
2017-04-12 11:28:47 -0700

    more log messages

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ var db = require('./models');
 // generate a new express app and call it 'app'
 var app = express();
 
+
+////////////////////
+//  MIDDLEWARE
+///////////////////
+
+
 // serve static files in public
 app.use(express.static('public'));
 
@@ -23,12 +29,22 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+// custom middleware to console.log some helpful information
+//   in terminal every time we get a request
+function logRequestInfo(req, res, next){
+  console.log(`\nRECEIVED REQUEST : ${req.method} ${req.url}`);
+  console.log('query params:', req.query);
+  console.log('body:', req.body);
+  // request url parameters haven't been decided yet
+  //  so we'll have to log them inside any routes where
+  //  we want to use them
+  next();
+}
+app.use(logRequestInfo);
+
 ////////////////////
 //  ROUTES
 ///////////////////
-
-
-
 
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
@@ -47,12 +63,14 @@ app.get('/api/books', function (req, res) {
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
+  console.log('request url params:', req.params)
   db.Book.findOne({_id: req.params.id }, function(err, data) {
     res.json(data);
   });
 });
 
 // create new book
+// AUTHOR MUST EXIST ALREADY!
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
   var newBook = new db.Book({
@@ -63,35 +81,39 @@ app.post('/api/books', function (req, res) {
   // find the author from req.body
   db.Author.findOne({name: req.body.author}, function(err, author){
     if (err) {
-      return console.log(err);
-    }
-    // if that author doesn't exist yet, create a new one
-    if (author === null) {
-      db.Author.create({name:req.body.author, alive:true}, function(err, newAuthor) {
-        createBookWithAuthorAndRespondTo(newBook, newAuthor, res);
-      });
+      console.log(err.message);
+      res.status(500).send();
     } else {
-      createBookWithAuthorAndRespondTo(newBook, author, res);
+      console.log('author is:', author);
+      if (author === null){
+        // this author doesn't exist in our database yet
+        // let's log an informative error message
+        console.log(`book create error: author ${req.body.author} not found - create author first!`);
+        res.status(500).send();
+      } else {
+        // found the author!
+        // add this author to the book
+        newBook.author = author;
+        // save newBook to database
+        newBook.save(function(err, book){
+          if (err) {
+            console.log('book save error:', err.message);
+            res.status(500).send();
+          } else {
+            console.log('saved book:', book );
+            // send back the book!
+            res.json(book);
+          }
+        });
+      }
     }
   });
 });
 
-function createBookWithAuthorAndRespondTo(book, author, res) {
-  // add this author to the book
-  book.author = author;
-  // save newBook to database
-  book.save(function(err, book){
-    if (err) {
-      return console.log("save error: " + err);
-    }
-    console.log("saved ", book.title);
-    // send back the book!
-    res.json(book);
-  });
-}
 
 // delete book
 app.delete('/api/books/:id', function (req, res) {
+  console.log('request url params:', req.params)
   // get book id from url params (`req.params`)
   console.log('books delete', req.params);
   var bookId = req.params.id;
@@ -104,19 +126,20 @@ app.delete('/api/books/:id', function (req, res) {
 });
 
 
-// Create a character associated with a book
+// create a character associated with a book
 app.post('/api/books/:book_id/characters', function (req, res) {
   // Get book id from url params (`req.params`)
+  console.log('request url params:', req.params)
   var bookId = req.params.book_id;
   db.Book.findById(bookId)
     .populate('author')
     .exec(function(err, foundBook) {
       console.log(foundBook);
       if (err) {
-        res.status(500).json({error: err.message});
+        res.status(500).send();
       } else if (foundBook === null) {
         // Is this the same as checking if the foundBook is undefined?
-        res.status(404).json({error: "No Book found by this ID"});
+        res.status(404).send();
       } else {
         // push character into characters array
         foundBook.characters.push(req.body);
@@ -130,6 +153,7 @@ app.post('/api/books/:book_id/characters', function (req, res) {
 
 // Delete a character associated with a book
 app.delete('/api/books/:book_id/characters/:character_id', function (req, res) {
+  console.log('request url params:', req.params)
   // Get book id from url params (`req.params`)
   var bookId = req.params.book_id;
   var characterId = req.params.character_id;
@@ -139,9 +163,9 @@ app.delete('/api/books/:book_id/characters/:character_id', function (req, res) {
     .exec(function(err, foundBook) {
       if (err) {
         console.log('error!', err.message);
-        res.status(500).json({error: err.message});
+        res.status(500).send();
       } else if (foundBook === null) {
-        res.status(404).json({error: "No Book found by this ID"});
+        res.status(500).send();
       } else {
         // find the character by id
         var deletedCharacter = foundBook.characters.id(characterId);
@@ -156,6 +180,9 @@ app.delete('/api/books/:book_id/characters/:character_id', function (req, res) {
 });
 
 
+////////////////////
+//  LISTEN
+///////////////////
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening at http://localhost:3000/');
