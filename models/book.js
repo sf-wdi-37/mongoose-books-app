737e9b748cd880e1ca51a54578763b0bd576991a
bgveenstra <b.g.veenstra@gmail.com>
2017-04-14 12:35:52 -0700

    add author model and convert author to reference in book schema

--- models/book.js
+++ models/book.js
@@ -1,9 +1,18 @@
+// models/book.js
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// var Character = require('./character.js')
+// var characterSchema = Character.schema;
+
 var BookSchema = new Schema({
      title: String,
-     author: String,
+    //  author: String,
+     author: {
+       type: Schema.Types.ObjectId,
+       ref: 'Author'
+     },
+    //  characters: [characterSchema],
      image: String,
      releaseDate: String
 });
